daba72cf2ed9e8e30e5ef2c67953af11c79b96ab
Angela Chi <achi@ennead.com>
2022-07-15 17:55:37 -0400

    session3 skills

--- RevitAddinAcademy/Command.cs
+++ RevitAddinAcademy/Command.cs
@@ -7,6 +7,7 @@ using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Forms = System.Windows.Forms;
 
 #endregion
 
@@ -25,9 +26,88 @@ namespace RevitAddinAcademy
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            TaskDialog.Show("Hello", "This is my first command add-in");
+            Forms.OpenFileDialog dialog = new Forms.OpenFileDialog();
+            dialog.InitialDirectory = @"C:\";
+            dialog.Multiselect = true;
+            dialog.Filter = "Revit Files | *.rvt; *.rfa";
+
+            string filePath = "";
+            string[] filePaths;
+
+            if(dialog.ShowDialog() == Forms.DialogResult.OK)
+            {
+                //filePath = dialog.FileName;
+                filePaths = dialog.FileNames;
+            }
+
+            Forms.FolderBrowserDialog folderDialog = new Forms.FolderBrowserDialog();
+
+            string folderPath = "";
+
+            if(folderDialog.ShowDialog() == Forms.DialogResult.OK)
+            {
+                folderPath = folderDialog.SelectedPath;
+            }
+
+            Tuple<string, int> t1 = new Tuple<string, int>("string 1", 55);
+            Tuple<string, int> t2 = new Tuple<string, int>("string 2", 155);
+
+            //TestStruct struct1;
+            //struct1.Name = "Structure 1";
+            //struct1.Value = 100;
+            //struct1.Value2 = 10.5;
+
+            TestStruct struct2 = new TestStruct("Structure 1", 10, 1004.4);
+
+            List<TestStruct> structList = new List<TestStruct>();
+            structList.Add(struct2);
+
+
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(ViewFamilyType));
+
+            ViewFamilyType curVFT = null;
+            ViewFamilyType curRCPVFT = null;
+            foreach(ViewFamilyType curElem in collector)
+            {
+                if(curElem.ViewFamily == ViewFamily.FloorPlan)
+                {
+                    curVFT = curElem;
+                }
+                else if(curElem.ViewFamily == ViewFamily.CeilingPlan)
+                {
+                    curRCPVFT = curElem;
+                }
+            }
+
+            using(Transaction t = new Transaction(doc))
+            {
+                Level newLevel = Level.Create(doc, 100);
+                ViewPlan curPlan = ViewPlan.Create(doc, curVFT.Id, newLevel.Id);
+                ViewPlan curRCP = ViewPlan.Create(doc, curRCPVFT.Id, newLevel.Id);
+                curRCP.Name = curRCP.Name + " RCP";
+
+            }
+
+
+
 
             return Result.Succeeded;
+        }
+
+        internal struct TestStruct
+        {
+            public string Name;
+            public int Value;
+            public double Value2;
+
+            public TestStruct(string name, int value, double value2)
+            {
+                Name = name;
+                Value = value;
+                Value2 = value2;
+            }
         }
     }
 }
